90d0d101fd03802b01f2e9ca64deb5702de00565
Ege Gungordu <egegungorduu@hotmail.com>
2024-04-25 01:18:19 +0300

    fix: lint error

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,7 +3,7 @@ import AccountDropdown from "./account-dropdown";
 export default function Sidebar() {
   return (
     <div className="h-full min-h-screen border-r w-64 bg-stone-100 flex flex-col p-2">
-      <AccountDropdown />
+      {/*<AccountDropdown /> */}
     </div>
   );
 }
