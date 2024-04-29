8dea4f14a59b71b77e34060baa413d07f5f30d77
Ege Gungordu <egegungorduu@hotmail.com>
2024-04-30 01:20:19 +0300

    fix: friends page session protection

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -12,12 +12,16 @@ export default async function Friends() {
     redirect("/onboarding");
   }
 
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <main className="mx-auto py-8 px-2 pt-20 max-w-screen-md">
       <div className="flex items-center justify-between">
         <h1 className="font-bold text-lg">Friends</h1>
 
-        <AddFriendButton username={session.user.username}/>
+        <AddFriendButton username={session.user.username || ""}/>
       </div>
 
       <div className="my-6" />
