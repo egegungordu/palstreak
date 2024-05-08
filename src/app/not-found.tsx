bbfaca4296feeea9cc93af28978602e7a97c09c1
Ege Gungordu <egegungorduu@hotmail.com>
2024-05-09 01:32:55 +0300

    chore: make 404 page fit rest of the layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,19 @@
-import Link from 'next/link'
- 
+import RightSidebarEmpty from "@/components/right-sidebar-empty";
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div className='flex min-h-96 justify-center items-center'>
-      <div className='flex flex-col items-center'>
-        <h1 className='text-4xl font-bold'>404</h1>
-        <h2 className='text-2xl font-semibold'>Page Not Found</h2>
-        <Link href='/' className='text-blue-500 mt-4'>
-          Go back home
-        </Link>
-      </div>
-    </div>
-  )
+    <>
+      <main className="w-full pt-8 pb-4 mx-auto lg:mx-0 px-4 md:px-2 max-w-screen-sm flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl font-bold">404</h1>
+          <h2 className="text-2xl font-semibold">Page Not Found</h2>
+          <Link href="/" className="text-blue-500 mt-4">
+            Go back home
+          </Link>
+        </div>
+      </main>
+      <RightSidebarEmpty />
+    </>
+  );
 }
