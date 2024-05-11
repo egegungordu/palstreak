c15e33a90484cfb2f2650646fd4848250c3ff9bf
Ege Gungordu <egegungorduu@hotmail.com>
2024-05-11 03:22:26 +0300

    feat: show habits on user pages

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema';
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
 import { type PostgresJsDatabase } from "drizzle-orm/postgres-js";
 
 // Fix for "sorry, too many clients already"
@@ -14,7 +14,7 @@ let db: PostgresJsDatabase<typeof schema>;
 if (process.env.NODE_ENV === "production") {
   db = drizzle(postgres(process.env.DATABASE_URL));
 } else {
-  if (!global.db) global.db = drizzle(postgres(process.env.DATABASE_URL));
+  if (!global.db) global.db = drizzle(postgres(process.env.DATABASE_URL), { schema });
   db = global.db;
 }
 
